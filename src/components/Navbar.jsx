cc637605c5a4b4d674c8c2fa8457ad608f7773b0
ashnadsilva23 <ashnadsilva2309@gmail.com>
2024-06-07 22:35:23 +0530

    project completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,7 @@ const Navbar = () => {
           <Link class="nav-link" to="/search">Search product</Link>
         </li>
         <li class="nav-item">
-          <Link class="nav-link" href="/delete">Delete Product</Link>
+          <Link class="nav-link" to="/delete">Delete Product</Link>
         </li>
         <li class="nav-item">
           <Link class="nav-link " to="/view">View_All</Link>
